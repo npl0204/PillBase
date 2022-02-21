eb4085aa57f2c67352b8a95e36acdfb96c7c7014
Katie Wu <2733-kw987@users.noreply.gitlab.cci.drexel.edu>
2022-02-21 06:21:59 +0000

    Set Reminder: dosage.dart (Update)

--- dosage.dart
+++ dosage.dart
@@ -1,45 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyState createState() => _MyState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _MyState extends State<MyApp> {
-  TextEditingController dosageController = TextEditingController();
-  String Dosage = '';
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Dosage'),
-          ),
-          body: Center(
-              child: Column(children: <Widget>[
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const TextFieldScreen(),
+    );
+  }
+}
+
+class TextFieldScreen extends StatefulWidget {
+  const TextFieldScreen({Key? key}) : super(key: key);
+
+  @override
+  _TextFieldScreenState createState() => _TextFieldScreenState();
+}
+
+class _TextFieldScreenState extends State<TextFieldScreen> {
+  final _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String dosage = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _formKey,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(dosage),
             Container(
-                margin: const EdgeInsets.all(20),
-                child: TextField(
-                  controller: dosageController,
-                  decoration: const InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Dosage',
-                    hintText: "Ex. 1",
-                  ),
-                  keyboardType: TextInputType.number,
-                  maxLength: 2,
-                  onChanged: (text) {
+              child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromRGBO(197, 199, 204, 100)),
+                      borderRadius: BorderRadius.all(Radius.circular(6))),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromRGBO(197, 199, 204, 100)),
+                      borderRadius: BorderRadius.all(Radius.circular(6))),
+                  hintText: 'Ex. 1',
+                  hintStyle:
+                      TextStyle(color: Color.fromRGBO(203, 207, 218, 100)),
+                  labelText: 'Dosage',
+                  labelStyle: TextStyle(
+                      color: Color.fromRGBO(99, 107, 133, 100),
+                      fontStyle: FontStyle.normal,
+                      fontSize: 15),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  floatingLabelStyle:
+                      TextStyle(color: Color.fromRGBO(99, 107, 133, 100)),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                maxLength: 2,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'test';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              padding: const EdgeInsets.all(32),
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: Text("verify"),
+                //color: Color.fromARGB(255, 223, 110, 144),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
                     setState(() {
-                      Dosage = text;
+                      dosage = _controller.text;
                     });
-                  },
-                )),
-          ]))),
+                  }
+                },
+              ),
+              padding: const EdgeInsets.all(32),
+            )
+          ]),
     );
   }
 }
