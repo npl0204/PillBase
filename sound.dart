12ca13a3c718cc15ae20e520bcdab9a7da862c94
Amalia Sorribes <as5628@drexel.edu>
2022-04-12 02:53:06 +0000

    Replace sound.dart

--- sound.dart
+++ sound.dart
@@ -1,20 +1,21 @@
-import 'package: rxdart/rxdart.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_native_timezone/flutter_native_timeone.dart';
 
 //notification
 class NotificationApp {
   static final _notifications = FlutterLocalNotificationPlugin();
-  static final onNotifications = BehaviorSubject<String?>();
+  static final onNotifications = BehaviorSubject();
+
+  static get styleInformation => null;
 
   static Future _notificationDetails() async{
     
     //sound
     //Download: https://samplefocus.com/samples/alert-sound-fx-announcement-pulse
-    final sound = 'notification_sound.wav';
+    const sound = 'notification_sound.wav';
     //need to add the sound as a new file called raw in the folder android/app/src/main/res in the project
-    //need to configure folder los open it with xcode -> create a new file in runner called resources and add the sound
+    //need to configure folder ios open it with xcode -> create a new file in runner called resources and add the sound
 
     return NotificationDetails(
       android: AndroidNotificationDetails(
@@ -76,4 +77,41 @@ class NotificationApp {
     String? body,
     String? payload,
   })
+
+
+  // ignore: non_constant_identifier_names
+  static RawResourceAndroidNotificationSound(String first) {}
+
+  // ignore: non_constant_identifier_names
+  static AndroidInitializationSettings(String s) {}
+
+  // ignore: non_constant_identifier_names
+  static IOSINitializationSettings() {}
+
+  // ignore: non_constant_identifier_names
+  static InitializationSettings({android, iOS}) {}}
+
+mixin Importance {
+  // ignore: prefer_typing_uninitialized_variables
+  static var max;
+}
+
+class IOSNotificationDetails {
+}
+
+class AndroidNotificationDetails {
+  AndroidNotificationDetails(String s, {importance, sound, required bool enableVibration, styleInformation});
+}
+
+class NotificationDetails {
+}
+
+class BehaviorSubject {
+  void add(payload) {}
+}
+
+class FlutterLocalNotificationPlugin {
+  initialize(settings, {required Future<void> Function(dynamic payload) onSelectNotification}) {}
+
+  show(int id, String? title, String? body, param3, {String? payload}) {}
 }
