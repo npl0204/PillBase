9f15e4f87b33de0a0c01f23712f624bfa13608ae
Katie Wu <2733-kw987@users.noreply.gitlab.cci.drexel.edu>
2022-04-18 23:50:58 +0000

    UPDATED File: View list of reminders: View reminder data

--- main.dart
+++ main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Med {
+  static var medicines = [
+    'Medicine #1',
+    'Medicine #2',
+    'Medicine #3',
+    'Medicine #4',
+    'Medicine #5',
+    'Medicine #6'
+  ];
+}
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +32,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Medicine {}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -35,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
     'Medicine #3',
     'Medicine #4',
     'Medicine #5',
-    'Medicine #5'
+    'Medicine #6'
   ];
 
   final dose = [
@@ -54,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 232, 101, 58),
+        backgroundColor: const Color.fromARGB(255, 232, 101, 58),
         title: Text(widget.title),
         centerTitle: true,
         elevation: 0,
@@ -64,20 +78,69 @@ class _MyHomePageState extends State<MyHomePage> {
           itemBuilder: (context, index) {
             return Card(
               child: ListTile(
-                tileColor: Color.fromARGB(255, 244, 246, 245),
-                contentPadding: EdgeInsets.symmetric(horizontal: 5.0),
-                onTap: () {},
+                tileColor: const Color.fromARGB(255, 244, 246, 245),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 5.0),
                 title: Text(medicines[index]),
                 subtitle: Text(dose[index]),
                 trailing: Text(time[index]),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(medicines[index]),
+                    ),
+                  );
+                },
               ),
             );
           }),
-      //floatingActionButton: FloatingActionButton(
-      //onPressed: _incrementCounter,
-      //tooltip: 'Increment',
-      //child: const Icon(Icons.add),
-      //), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen(String medicine, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Reminder #"),
+        ),
+        body: RichText(
+          text: const TextSpan(
+            //text: "Medicine Name",
+            style: TextStyle(
+                fontStyle: FontStyle.normal,
+                fontSize: 18,
+                height: 2,
+                fontWeight: FontWeight.bold),
+
+            children: <TextSpan>[
+              TextSpan(text: "Medicine Name"),
+              TextSpan(
+                  text: "\n# Tablets",
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 15,
+                      height: 1.5,
+                      fontWeight: FontWeight.normal)),
+              TextSpan(
+                  text: "\n 7:00PM",
+                  style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 15,
+                      height: 1.5,
+                      fontWeight: FontWeight.normal)),
+              TextSpan(
+                  text: "\n Created 4/12/22 at 4:45PM",
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 14,
+                      height: 49,
+                      fontWeight: FontWeight.normal))
+            ],
+          ),
+        ));
   }
 }
